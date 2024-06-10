47be6d6247f0b6773a63c13392bc749ed76f6c57
Melike Sultan CAN <melikesultan027@gmail.com>
2024-06-10 11:59:35 +0300

    Initial Commit

---
+++ ChatGeminiAssissant/ChatGeminiAssissantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGeminiAssissantApp.swift
+//  ChatGeminiAssissant
+//
+//  Created by Melike Sultan CAN on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatGeminiAssissantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
